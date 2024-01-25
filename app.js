80014945a1a1b8754918661a21fa4b1c9ad11831
laismilenam <140639353+laismilenam@users.noreply.github.com>
2024-01-25 11:53:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 2;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
-let tentantivas = 1;
-// enquanto chute não for ingual ao numeroSecreto
-while (chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 10');
+let tentativas = 1;
 
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 100');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentantivas}`);
+        break;
     } else {
         if (chute > numeroSecreto) {
-        alert (`O número secreto é menor que ${chute}`);
-
+            alert(`O número secreto é menor que ${chute}`);
         } else {
-        alert (`O número secreto é maior que ${chute}`);
+            alert(`O número secreto é maior que ${chute}`);
         }
-        tentantivas++;
-    } 
+        // tentativas = tentativas + 1;
+        tentativas++;
+    }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+// if (tentativas > 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// }
