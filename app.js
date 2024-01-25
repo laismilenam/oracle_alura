d7ecedf0f1bff5439e344902388575fc81011985
laismilenam <140639353+laismilenam@users.noreply.github.com>
2024-01-25 11:27:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 9;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = 2;
+console.log(numeroSecreto);
+let chute;
+let tentantivas = 1;
+// enquanto chute não for ingual ao numeroSecreto
+while (chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 10');
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você errou :(')
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentantivas}`);
+    } else {
+        if (chute > numeroSecreto) {
+        alert (`O número secreto é menor que ${chute}`);
+
+        } else {
+        alert (`O número secreto é maior que ${chute}`);
+        }
+        tentantivas++;
+    } 
 }
